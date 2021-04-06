9a4ee448138751418c39d0c2a7b47dccd0bb661c
Tangnameless <tang0079@foxmail.com>
2021-04-06 21:14:14 +0800

    更新至P8
    增加了更新操作

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -4,6 +4,13 @@
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="250"></el-table-column>
       <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small" 
+          @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -11,19 +18,19 @@
 <script>
 export default {
   data() {
-    return{
-      items: []
-    }
+    return {
+      items: [],
+    };
   },
-  methods:{
-    async fetch(){
-      const res = await this.$http.get('categories');
+  methods: {
+    async fetch() {
+      const res = await this.$http.get("categories");
       this.items = res.data;
-    }
+    },
   },
 
   created() {
     this.fetch();
-  }
-}
+  },
+};
 </script>
