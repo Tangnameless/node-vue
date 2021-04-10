2057cf3a084ae6a7800d6572ccb43732f0f1b5da
Tangnameless <tang0079@foxmail.com>
2021-04-10 17:05:28 +0800

    更新至P12
    增加物品的curd

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="about">
-    <h1>{{id? '编辑': '新建'}}分类</h1>
+    <h1>{{id? '编辑': '新建'}}物品</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <el-form-item label="上级分类">
-        <el-select v-model="model.parent">
-          <el-option 
-          v-for="item in parents"
-          :key="item._id"
-          :label="item.name"
-          :value="item._id"></el-option>
-        </el-select>
-      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
+      </el-form-item>
+        <el-form-item label="图标">
+        <el-input v-model="model.icon"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -29,41 +23,32 @@ export default {
   data() {
     return {
       model: {},
-      parents:[],
     }
   },
   methods: {
     async save() {
       let res
       if(this.id){
-        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
+        res = await this.$http.put(`rest/items/${this.id}`, this.model);
       }else{
-        res = await this.$http.post("rest/categories", this.model);
+        res = await this.$http.post("rest/items", this.model);
       }
       if (res) {
-        this.$router.push("/categories/list");
+        this.$router.push("/items/list");
         this.$message({
           type: "success",
           message: "保存成功",
         });
       }
     },
-
+    
     async fetch() {
-      const res = await this.$http.get(`rest/categories/${this.id}`) // 模板字符串（` `）
+      const res = await this.$http.get(`rest/items/${this.id}`) // 模板字符串（` `）
       this.model = res.data
     },
-
-    
-    async fetchPatents() {
-      const res = await this.$http.get(`rest/categories`) // 模板字符串（` `）
-      this.parents = res.data
-    },
-
   },
 
   created() {
-    this.fetchPatents()
     this.id && this.fetch();
   }
 };
