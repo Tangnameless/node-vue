3fecc557facda889948f24587c7bbf42b374127b
Tangnameless <tang0079@foxmail.com>
2021-04-10 21:10:56 +0800

    更新至P16
    增加了英雄编辑

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -1,8 +1,13 @@
 <template>
   <el-container style="height: 100vh">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router :default-openeds="['1']" unique-opened :default-active="$route.path">
-       <el-submenu index="1">
+      <el-menu
+        router
+        :default-openeds="['1']"
+        unique-opened
+        :default-active="$route.path"
+      >
+        <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-message"></i>内容管理
           </template>
@@ -15,6 +20,11 @@
             <template slot="title">物品</template>
             <el-menu-item index="/items/create">新建物品</el-menu-item>
             <el-menu-item index="/items/list">物品列表</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group>
+            <template slot="title">英雄</template>
+            <el-menu-item index="/heros/create">新建英雄</el-menu-item>
+            <el-menu-item index="/heros/list">英雄列表</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
